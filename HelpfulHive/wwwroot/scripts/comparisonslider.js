ba1309be3d75abc3eec0116996cddb5d9f700195
bestsergik <sergynia88@mail.ru>
2023-10-23 21:09:34 +0300

    add universal xml parser

---
+++ HelpfulHive/wwwroot/scripts/comparisonslider.js
@@ -0,0 +1,38 @@
+﻿
+window.sliderFunctions = {
+    initializeSlider: function () {
+        const slider = document.getElementById('slider');
+        const panelLeft = document.getElementById('panel-left');
+        const panelRight = document.getElementById('panel-right');
+        const container = document.getElementById('split-container');
+
+        slider.addEventListener('mousedown', function (e) {
+            e.preventDefault();
+
+            const startX = e.clientX;
+            const startLeft = slider.offsetLeft;
+
+            function onMouseMove(e) {
+                let newLeft = startLeft + e.clientX - startX;
+
+                if (newLeft < 0) {
+                    newLeft = 0;
+                } else if (newLeft > container.offsetWidth - slider.offsetWidth) {
+                    newLeft = container.offsetWidth - slider.offsetWidth;
+                }
+
+                panelLeft.style.width = `${newLeft}px`;
+                panelRight.style.width = `${container.offsetWidth - newLeft - slider.offsetWidth}px`;
+                slider.style.left = `${newLeft}px`;
+            }
+
+            function onMouseUp() {
+                document.removeEventListener('mousemove', onMouseMove);
+                document.removeEventListener('mouseup', onMouseUp);
+            }
+
+            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mouseup', onMouseUp);
+        });
+    }
+};
--- HelpfulHive/wwwroot/scripts/watchinginsert.js
+++ HelpfulHive/wwwroot/scripts/watchinginsert.js
@@ -1,5 +1,24 @@
-﻿function handlePaste(editor, dotnetReference) {
-    editor.onDidPaste(() => {
-        dotnetReference.invokeMethodAsync('FormatXml');
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var slider = document.getElementById('slider');
+    var isDragging = false;
+
+    slider.addEventListener('mousedown', function () {
+        isDragging = true;
     });
-}
+
+    document.addEventListener('mouseup', function () {
+        isDragging = false;
+    });
+
+    document.addEventListener('mousemove', function (event) {
+        if (isDragging) {
+            var leftWidth = event.clientX;
+            var rightWidth = window.innerWidth - event.clientX - slider.offsetWidth;
+            var leftContainer = slider.previousElementSibling;
+            var rightContainer = slider.nextElementSibling;
+
+            leftContainer.style.flexGrow = leftWidth;
+            rightContainer.style.flexGrow = rightWidth;
+        }
+    });
+});
