51034da11e9be8649e286268043a37806a91a382
bestsergik <sergynia88@mail.ru>
2023-10-31 23:23:18 +0300

    add correct saving text with images in content

--- HelpfulHive/Services/ImageService.cs
+++ HelpfulHive/Services/ImageService.cs
@@ -2,6 +2,83 @@
 {
     public class ImageService
     {
+
+        private readonly IWebHostEnvironment _env;
+
+        public ImageService(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public async Task<string> SaveImageAsync(string base64Image, String imagePath)
+        {
+            Console.WriteLine("Start SaveImageAsync");
+
+            if (string.IsNullOrEmpty(base64Image))
+                throw new ArgumentNullException(nameof(base64Image));
+
+            byte[] bytes;
+            // Проверяем, содержит ли строка разделитель
+            if (base64Image.Contains(","))
+            {
+                // Разделение строки на части
+                var parts = base64Image.Split(',');
+                if (parts.Length != 2)
+                {
+                    Console.WriteLine("Invalid base64 image string. Parts length: " + parts.Length);
+                    throw new InvalidOperationException("Invalid base64 image string.");
+                }
+
+                // Конвертация base64 строки в массив байтов
+                try
+                {
+                    bytes = Convert.FromBase64String(parts[1]);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Provided string is not a valid Base64 string.");
+                    throw new InvalidOperationException("Provided string is not a valid Base64 string.", ex);
+                }
+            }
+            else
+            {
+                // Строка не содержит разделителя, пытаемся конвертировать её целиком
+                try
+                {
+                    bytes = Convert.FromBase64String(base64Image);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Provided string is not a valid Base64 string.");
+                    throw new InvalidOperationException("Provided string is not a valid Base64 string.", ex);
+                }
+            }
+
+            // Создание уникального имени файла
+            var fileName = Guid.NewGuid().ToString() + ".png";
+            Console.WriteLine($"Generated filename: {fileName}");
+
+            // Создание пути к файлу
+            var filePath = Path.Combine(_env.WebRootPath, imagePath, fileName);
+            Console.WriteLine($"File path: {filePath}");
+
+            // Создание директории, если она не существует
+            if (!Directory.Exists(Path.Combine(_env.WebRootPath, imagePath)))
+            {
+                Directory.CreateDirectory(Path.Combine(_env.WebRootPath, imagePath));
+                Console.WriteLine("Directory created");
+            }
+
+            // Запись байтов в файл
+            await File.WriteAllBytesAsync(filePath, bytes);
+            Console.WriteLine("Image saved");
+
+            Console.WriteLine("End SaveImageAsync");
+            return filePath;
+        }
+
+
+
         public Dictionary<string, List<string>> GetImagesByFolders()
         {
             var imagesFolder = "wwwroot/images";
